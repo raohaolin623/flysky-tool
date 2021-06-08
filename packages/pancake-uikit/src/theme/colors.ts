89b544e32896028aa44cd8389353edd5f1ab390e
uloz <uloz@gmail.com>
2021-06-09 01:06:57 +0800

    change style

--- packages/pancake-uikit/src/theme/colors.ts
+++ packages/pancake-uikit/src/theme/colors.ts
@@ -44,11 +44,42 @@ export const lightColors: Colors = {
   },
 };
 
+// export const darkColors: Colors = {
+//   ...baseColors,
+//   ...additionalColors,
+//   secondary: "#9A6AFF",
+//   background: "#08060B",
+//   backgroundDisabled: "#3c3742",
+//   backgroundAlt: "#27262c",
+//   cardBorder: "#383241",
+//   contrast: "#FFFFFF",
+//   dropdown: "#1E1D20",
+//   dropdownDeep: "#100C18",
+//   invertedContrast: "#191326",
+//   input: "#372F47",
+//   inputSecondary: "#262130",
+//   primaryDark: "#0098A1",
+//   tertiary: "#353547",
+//   text: "#F4EEFF",
+//   textDisabled: "#666171",
+//   textSubtle: "#B8ADD2",
+//   disabled: "#524B63",
+//   gradients: {
+//     bubblegum: "linear-gradient(139.73deg, #313D5C 0%, #3D2A54 100%)",
+//     inverseBubblegum: "linear-gradient(139.73deg, #3D2A54 0%, #313D5C 100%)",
+//     cardHeader: "linear-gradient(166.77deg, #3B4155 0%, #3A3045 100%)",
+//     blue: "linear-gradient(180deg, #00707F 0%, #19778C 100%)",
+//     violet: "linear-gradient(180deg, #6C4999 0%, #6D4DB2 100%)",
+//     violetAlt: "linear-gradient(180deg, #434575 0%, #66578D 100%)",
+//     gold: "linear-gradient(180deg, #FFD800 0%, #FDAB32 100%)",
+//   },
+// };
+
 export const darkColors: Colors = {
   ...baseColors,
   ...additionalColors,
   secondary: "#9A6AFF",
-  background: "#08060B",
+  background: "#222222",
   backgroundDisabled: "#3c3742",
   backgroundAlt: "#27262c",
   cardBorder: "#383241",
